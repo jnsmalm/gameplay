37b7eb27423c4589b5d903ec9cb83a443a7eebd0
Jens Malmborg <jnsmalm@gmail.com>
2017-03-28 22:11:26 +0200

    Added more stuff to openal

--- openal/index.d.ts
+++ openal/index.d.ts
@@ -1,7 +1,20 @@
 declare module "gameplay/openal" {
+  function bufferData(
+    buffer: number, format: number, data: ArrayBufferView, rate: number): void
   function closeDevice(device: number): void
+  function createBuffer(): number
   function createContext(): number
+  function createSource(): number
   function destroyContext(context: number): void
+  function getError(): number
+  function getSourcei(source: number, param: number): number
   function makeContextCurrent(context: number): void
   function openDevice(): number
+  function source3f(
+    source: number, format: number, v0: number, v1: number, v2: number): void
+  function sourcef(source: number, format: number, v0: number): void
+  function sourcei(source: number, format: number, buffer: number): void
+  function sourcePause(source: number): void
+  function sourcePlay(source: number): void
+  function sourceStop(source: number): void
 }
--- openal/openal.cc
+++ openal/openal.cc
@@ -15,6 +15,25 @@ using v8::Value;
 using v8::Isolate;
 using v8::String;
 using v8::Exception;
+using v8::ArrayBufferView;
+
+static void BufferData(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  if (!args[0]->IsNumber() || !args[1]->IsNumber() ||
+      !args[2]->IsArrayBufferView() || !args[3]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+  ALuint buffer = static_cast<ALuint>(args[0]->IntegerValue());
+  ALuint format = static_cast<ALuint>(args[1]->IntegerValue());
+  Local<ArrayBufferView> data = Local<ArrayBufferView>::Cast(args[2]);
+  ALuint rate = static_cast<ALuint>(args[3]->IntegerValue());
+
+  alBufferData(buffer, format, data->Buffer()->GetContents().Data(), 
+    static_cast<ALsizei>(data->ByteLength()), rate);
+}
 
 static void CloseDevice(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
@@ -28,6 +47,12 @@ static void CloseDevice(const FunctionCallbackInfo<Value>& args) {
 
   ALCdevice* device = reinterpret_cast<ALCdevice*>(handle);
   alcCloseDevice(device);
+}
+
+static void CreateBuffer(const FunctionCallbackInfo<Value>& args) {
+  ALuint buffer;
+  alGenBuffers((ALuint)1, &buffer);
+  args.GetReturnValue().Set(buffer);
 }
 
 static void CreateContext(const FunctionCallbackInfo<Value>& args) {
@@ -45,6 +70,12 @@ static void CreateContext(const FunctionCallbackInfo<Value>& args) {
     Number::New(isolate, (uint64_t)alcCreateContext(device, NULL)));
 }
 
+static void CreateSource(const FunctionCallbackInfo<Value>& args) {
+  ALuint source;
+  alGenSources((ALuint)1, &source);
+  args.GetReturnValue().Set(source);
+}
+
 static void DestroyContext(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
 
@@ -57,6 +88,27 @@ static void DestroyContext(const FunctionCallbackInfo<Value>& args) {
 
   ALCcontext* context = reinterpret_cast<ALCcontext*>(handle);
   alcDestroyContext(context);
+}
+
+static void GetError(const FunctionCallbackInfo<Value>& args) {
+  args.GetReturnValue().Set(alGetError());
+}
+
+static void GetSourcei(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+  ALuint source = static_cast<ALuint>(args[0]->IntegerValue());
+  ALenum param = static_cast<ALenum>(args[1]->IntegerValue());
+
+  ALint value;
+  alGetSourcei(source, param, &value);
+
+  args.GetReturnValue().Set(value);
 }
 
 static void MakeContextCurrent(const FunctionCallbackInfo<Value>& args) {
@@ -93,13 +145,114 @@ static void OpenDevice(const FunctionCallbackInfo<Value>& args) {
   args.GetReturnValue().Set(Number::New(isolate, (uint64_t)device));
 }
 
+static void Source3f(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  if (!args[0]->IsNumber() || !args[1]->IsNumber() ||
+      !args[2]->IsNumber() || !args[3]->IsNumber() ||
+      !args[4]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+  ALuint source = static_cast<ALuint>(args[0]->IntegerValue());
+  ALenum param = static_cast<ALenum>(args[1]->IntegerValue());
+  ALfloat v0 = static_cast<ALfloat>(args[2]->NumberValue());
+  ALfloat v1 = static_cast<ALfloat>(args[3]->NumberValue());
+  ALfloat v2 = static_cast<ALfloat>(args[4]->NumberValue());
+
+  alSource3f(source, param, v0, v1, v2);
+}
+
+static void Sourcef(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  if (!args[0]->IsNumber() || !args[1]->IsNumber() ||
+      !args[2]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+  ALuint source = static_cast<ALuint>(args[0]->IntegerValue());
+  ALenum param = static_cast<ALenum>(args[1]->IntegerValue());
+  ALfloat v0 = static_cast<ALfloat>(args[2]->NumberValue());
+
+  alSourcef(source, param, v0);
+}
+
+static void Sourcei(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  if (!args[0]->IsNumber() || !args[1]->IsNumber() ||
+      !args[2]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+  ALuint source = static_cast<ALuint>(args[0]->IntegerValue());
+  ALenum param = static_cast<ALenum>(args[1]->IntegerValue());
+  ALuint buffer = static_cast<ALuint>(args[2]->IntegerValue());
+
+  alSourcei(source, param, buffer);
+}
+
+static void SourcePause(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  if (!args[0]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+  ALuint source = static_cast<ALuint>(args[0]->IntegerValue());
+
+  alSourcePause(source);
+}
+
+static void SourcePlay(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  if (!args[0]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+  ALuint source = static_cast<ALuint>(args[0]->IntegerValue());
+
+  alSourcePlay(source);
+}
+
+static void SourceStop(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+
+  if (!args[0]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+        String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+  ALuint source = static_cast<ALuint>(args[0]->IntegerValue());
+
+  alSourceStop(source);
+}
+
 void Initialize(
     Local<Object> target, Local<Value> unused, Local<Context> context) {
+  NODE_SET_METHOD(target, "bufferData", BufferData);
   NODE_SET_METHOD(target, "closeDevice", CloseDevice);
+  NODE_SET_METHOD(target, "createBuffer", CreateBuffer);
   NODE_SET_METHOD(target, "createContext", CreateContext);
+  NODE_SET_METHOD(target, "createSource", CreateSource);
   NODE_SET_METHOD(target, "destroyContext", DestroyContext);
+  NODE_SET_METHOD(target, "getError", GetError);
+  NODE_SET_METHOD(target, "getSourcei", GetSourcei);
   NODE_SET_METHOD(target, "makeContextCurrent", MakeContextCurrent);
   NODE_SET_METHOD(target, "openDevice", OpenDevice);
+  NODE_SET_METHOD(target, "source3f", Source3f);
+  NODE_SET_METHOD(target, "sourcef", Sourcef);
+  NODE_SET_METHOD(target, "sourcei", Sourcei);
+  NODE_SET_METHOD(target, "sourcePause", SourcePause);
+  NODE_SET_METHOD(target, "sourcePlay", SourcePlay);
+  NODE_SET_METHOD(target, "sourceStop", SourceStop);
 }
 
 }  // namespace openal
