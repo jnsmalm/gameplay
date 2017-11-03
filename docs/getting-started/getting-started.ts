87b2684485676974571c37186c6cdb7b88018916
Jens Malmborg <jnsmalm@gmail.com>
2017-11-03 16:27:54 +0100

    Download cube3d.json for getting-started

--- docs/getting-started/getting-started.ts
+++ docs/getting-started/getting-started.ts
@@ -12,7 +12,7 @@ $.Game.init()
 
 let camera = $.Camera.createDefault($.Game.window)
 let shader = new $.BasicShader()
-let cube3d = $.Assimp.createModelFromFile("docs/_content/cube3d.json", shader)
+let cube3d = $.Assimp.createModelFromFile(__dirname + "/assets/cube3d.json", shader)
 
 $.Game.draw = () => {
   shader.setView(camera.getView())
@@ -25,8 +25,9 @@ $.Game.update = () => {
   cube3d.transform.localRotation.rotateY(1)
 }
 
-/*Run the code with the gameplay script (which uses the bundled Node.js runtime). 
-If everything has gone well you should see a spinning 3D cube.
+/*Download <a href="assets/cube3d.json" download>cube3d.json</a> to a folder 
+named "assets". Run the application with Gameplay.js (which uses the bundled 
+Node.js runtime). If everything has gone well you should see a spinning 3D cube.
 
 ```
 % ./gameplay cube3d.js
@@ -51,15 +52,15 @@ To run the above example in TypeScript you need to download the
 It's also recommended that you download 
 [Visual Studio Code](https://code.visualstudio.com). Then follow these steps:
 
-1. Start your favorite code editor (which supports TypeScript) and open the 
-  Gameplay.js folder
-2. Create a new file *cube3d.ts* with the same contents as *cube3d.js*
+1. Start your favorite code editor and open the Gameplay.js folder.
+2. Create a new file *cube3d.ts* with the same contents as *cube3d.js*.
 3. Change the line `const $ = require("gameplay/lib")` to `import * as $ from 
-  "gameplay/lib"`
-4. Type `tsc --init` in the terminal to generate a file *tsconfig.json*, 
-  also type `npm install @types/node` to install typings for Node.js
-5. Edit *tsconfig.json* and set module to "commonjs" and target to "es2017"
+  "gameplay/lib"`.
+4. Type `tsc --init` in the terminal to generate a file *tsconfig.json* which 
+  includes TypeScript configuration. Edit *tsconfig.json* and set module to 
+  "commonjs" and target to "es2017".
+5. Type `npm install @types/node` to install typings for Node.js.
 6. Type `tsc` (to compile) or `tsc -w` in the terminal (to compile and 
-  continuously check for file changes)
-7. TypeScript compiler should now have compiled *cube3d.ts* to *cube3d.js*
-8. Run the application with the gameplay script `% ./gameplay cube3d.js`*/
+  continuously check for file changes).
+7. TypeScript compiler should now have compiled *cube3d.ts* to *cube3d.js*.
+8. Run the application with the Gameplay.js `% ./gameplay cube3d.js`.*/
